00648633ac8872a12b360c95eae4eb03766f0f8e
Theo Natan <133386307+theonatangoes@users.noreply.github.com>
2025-03-20 14:36:19 -0300

    fix(errors): ajustar layout da página de erro 404
    - Foram realizadas melhorias no design da página de erro 404, incluindo a atualização da imagem de fundo e ajustes na estrutura do template. Essas alterações visam proporcionar uma experiência mais clara e intuitiva ao usuário ao acessar páginas inexistentes, tornando a navegação mais amigável.

--- resources/views/errors/error404.blade.php
+++ resources/views/errors/error404.blade.php
@@ -7,83 +7,80 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="flex items-center justify-center min-h-screen bg-slate-900 relative"> 
-        <img id="" class="" src="{{ asset('assets/images/logo404.png') }}" alt="">
-        <img id="" class="absolute w-full top-0 left-6" src="{{ asset('assets/images/bgError404.png') }}" alt="">
+    
+<div class="flex items-center justify-center min-h-[calc(100vh-140px)] bg-slate-900 relative">
+        
+        <div class="absolute top-4 left-4">
+            <img src="{{ asset('assets/images/logoHome.png') }}" alt="Logo Home" class="h-22">
+        </div>
+        <img id="" class="" src="{{ asset('assets/images/logo404.png') }}" alt="Logo 404">
     </div>
     
-     <!-- Footer  -->
+  <!-- Footer -->
     <footer class="bg-yellow-500 w-full p-5">
+        <div class="grid grid-cols-4 justify-items-center gap-4 text-slate-900">
+            <div class="p-4">
+                <img src="{{ asset('assets/images/logoFooter.png') }}" alt="">
+            </div>
 
-      <div class="grid grid-cols-4 justify-items-center gap-4 text-slate-900">
-          <div class="p-4">
-            <img src="{{ asset('assets/images/logoFooter.png') }}" alt="">
-          </div>
+            <div class="p-4">
+                <h1 class="inline-block text-xl font-medium relative mb-4">
+                    Gestão de Frota
+                    <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
+                </h1>
+                <ul class="grid gap-2">
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>DashBoard</li>
+                    </a>
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>Relatórios</li>
+                    </a>
+                </ul>
+            </div>
 
-          <div class="p-4">
+            <div class=" p-4">
+                <h1 class="inline-block text-xl font-medium relative mb-4">
+                    Veículos e motorista
+                    <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
+                </h1>
+                <ul class="grid gap-2">
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>Caminhões</li>
+                    </a>
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>Motoristas</li>
+                    </a>
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>Rotas</li>
+                    </a>
+                </ul>
+            </div>
 
-            <h1 class="inline-block text-xl font-medium relative mb-4">
-              Gestão de Frota
-              <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
-            </h1>
+            <div class="p-4">
+                <h1 class="inline-block text-xl font-medium relative mb-4">
+                    Suporte e contato
+                    <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
+                </h1>
+                <ul class="grid gap-2">
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>FAQ</li>
+                    </a>
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>Termos de Uso</li>
+                    </a>
+                    <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                        <li>Política de Privacidade</li>
+                    </a>
+                    <a class=" hover:text-slate-900 transition-all duration-300 ease-in-out hover:translate-x-1 text-purple-900 hover:underline" href="#">
+                        <li>suporte@fleetmax.com</li>
+                    </a>
+                </ul>
+            </div>
+        </div>
 
-            <ul class="grid gap-2">
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>DashBoard</li>
-              </a>
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>Relatórios</li>
-              </a>
-            </ul>
-            
-          </div>
-          <div class=" p-4">
-            <h1 class="inline-block text-xl font-medium relative mb-4">
-              Veículos e motorista
-              <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
-            </h1>
-
-            <ul class="grid gap-2">
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>Caminhões</li>
-              </a>
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>Motoristas</li>
-              </a>
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>Rotas</li>
-              </a>
-            </ul>
-          </div>
-
-          <div class="p-4">
-            <h1 class="inline-block text-xl font-medium relative mb-4">
-              Suporte e contato
-              <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
-            </h1>
-
-            <ul class="grid gap-2">
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>FAQ</li>
-              </a>
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>Termos de Uso</li>
-              </a>
-              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
-                <li>Política de Privacidade</li>
-              </a>
-              <a class=" hover:text-slate-900 transition-all duration-300 ease-in-out hover:translate-x-1 text-purple-900 hover:underline" href="#">
-                <li>suporte@fleetmax.com</li>
-              </a>
-            </ul>
-          </div>
-
-          </div>
-
-      <div class="text-right text-slate-900 mt-4 p-4">
-        <p>FleetMax © Alguns direitos reservados. 2025</p>
-      </div>
+        <div class="text-right text-slate-900 mt-4 p-4">
+            <p>FleetMax © Alguns direitos reservados. 2025</p>
+        </div>
     </footer>
-
 </body>
 </html>
