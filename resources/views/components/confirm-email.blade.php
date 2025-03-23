a9850f48e6c20e13bb5f01612da00af6927bfbc6
Ícaro Moreira <127455211+IcaroMoreir4@users.noreply.github.com>
2025-03-23 20:26:12 -0300

    feat: adicionar novos componentes de view

---
+++ resources/views/components/confirm-email.blade.php
@@ -0,0 +1,20 @@
+<div class="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50">>
+    <div class="confirmEmail flex flex-col items-center w-[36.5rem] bg-slate-800 p-5 rounded-xl shadow-lg">
+        <img class="mb-4" src="{{ asset('assets/images/icons/iconConfrimEmail.svg') }}" alt="">
+        <h1 class="text-white text-4xl font-bold mb-1.5">Confirme seu e-mail</h1>
+        <p class="text-white font-light mb-5 text-center">
+            Clique no botão abaixo para validar seu endereço de e-mail e confirmar 
+            que você é o proprietário desta conta.
+        </p>
+
+        <p class="text-white font-light mb-10">Caso contrário, ignore esta mensagem.</p>
+
+        <p class="text-sm text-yellow-500 font-light mb-3">
+            O link expirará em 24 horas. Se você tiver dúvidas, suporte@fleetmax.com
+        </p>
+
+        <button class="text-slate-800 bg-yellow-500 font-bold px-7 py-2.5 rounded-4xl transition-all duration-300 ease-in-out shadow-md hover:bg-yellow-400 hover:shadow-lg hover:scale-105 active:scale-95">
+            VALIDAR E FAZER LOGIN
+        </button>
+    </div>
+</div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,71 @@
+<footer class="bg-yellow-500 w-full p-5">
+
+      <div class="grid grid-cols-4 justify-items-center gap-4 text-slate-900">
+          <div class="p-4">
+            <img src="{{ asset('assets/images/logoFooter.png') }}" alt="">
+          </div>
+
+          <div class="p-4">
+
+            <h1 class="inline-block text-xl font-medium relative mb-4">
+              Gestão de Frota
+              <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
+            </h1>
+
+            <ul class="grid gap-2">
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>DashBoard</li>
+              </a>
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>Relatórios</li>
+              </a>
+            </ul>
+            
+          </div>
+          <div class=" p-4">
+            <h1 class="inline-block text-xl font-medium relative mb-4">
+              Veículos e motorista
+              <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
+            </h1>
+
+            <ul class="grid gap-2">
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>Caminhões</li>
+              </a>
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>Motoristas</li>
+              </a>
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>Rotas</li>
+              </a>
+            </ul>
+          </div>
+
+          <div class="p-4">
+            <h1 class="inline-block text-xl font-medium relative mb-4">
+              Suporte e contato
+              <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
+            </h1>
+
+            <ul class="grid gap-2">
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>FAQ</li>
+              </a>
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>Termos de Uso</li>
+              </a>
+              <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+                <li>Política de Privacidade</li>
+              </a>
+              <a class=" hover:text-slate-900 transition-all duration-300 ease-in-out hover:translate-x-1 text-purple-900 hover:underline" href="#">
+                <li>suporte@fleetmax.com</li>
+              </a>
+            </ul>
+          </div>
+
+          </div>
+
+      <div class="text-right text-slate-900 mt-4 p-4">
+        <p>FleetMax © Alguns direitos reservados. 2025</p>
+      </div>
+</footer>
