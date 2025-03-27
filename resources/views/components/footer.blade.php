34016f4d3998d6b6471157466a5dc946162dd2a9
Ícaro Moreira <127455211+IcaroMoreir4@users.noreply.github.com>
2025-03-27 16:58:51 -0300

    fix: corrigir rotas no footer e sidebar
    - Atualizadas as rotas nos links do footer e da sidebar para garantir que os direcionamentos sejam corretos.
    - Corrigido o href das âncoras, vinculando as rotas dinâmicas corretamente no Laravel.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,41 +1,37 @@
 <footer class="bg-yellow-500 w-full p-5">
-
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-4 text-slate-900">
     <div class="p-4">
       <img src="{{ asset('assets/images/logoFooter.png') }}" alt="">
     </div>
 
     <div class="p-4">
-
       <h1 class="inline-block text-xl font-medium relative mb-4">
         Gestão de Frota
         <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
       </h1>
-
       <ul class="grid gap-2">
-        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="{{ route('dashboard') }}">
           <li>DashBoard</li>
         </a>
-        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="{{ route('relatorios') }}">
           <li>Relatórios</li>
         </a>
       </ul>
-
     </div>
+
     <div class=" p-4">
       <h1 class="inline-block text-xl font-medium relative mb-4">
         Veículos e motorista
         <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
       </h1>
-
       <ul class="grid gap-2">
-        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="{{ route('caminhoes') }}">
           <li>Caminhões</li>
         </a>
-        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="{{ route('motoristas') }}">
           <li>Motoristas</li>
         </a>
-        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
+        <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="{{ route('rotas') }}">
           <li>Rotas</li>
         </a>
       </ul>
@@ -46,7 +42,6 @@
         Suporte e contato
         <span class="absolute inset-x-0 mx-auto left-0 -bottom-0.5 w-full h-0.5 bg-white"></span>
       </h1>
-
       <ul class="grid gap-2">
         <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
           <li>FAQ</li>
@@ -57,12 +52,11 @@
         <a class="transition-all duration-300 ease-in-out hover:translate-x-1 hover:text-purple-900 hover:underline decoration-purple-900" href="#">
           <li>Política de Privacidade</li>
         </a>
-        <a class=" hover:text-slate-900 transition-all duration-300 ease-in-out hover:translate-x-1 text-purple-900 hover:underline" href="#">
+        <a class="hover:text-slate-900 transition-all duration-300 ease-in-out hover:translate-x-1 text-purple-900 hover:underline" href="mailto:suporte@fleetmax.com">
           <li>suporte@fleetmax.com</li>
         </a>
       </ul>
     </div>
-
   </div>
 
   <div class="text-right text-slate-900 mt-4 p-4">
