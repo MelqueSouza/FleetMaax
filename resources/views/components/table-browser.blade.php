ecf9114bb67824f837137d8cfa7d30bfc956818d
Manoel Mesquita <143899261+ManoelmNeto@users.noreply.github.com>
2025-03-28 13:53:14 -0300

    Criação do componente TableBrowser
    Criado o componente TableBrowser.php para melhorar a reutilização e organização das tabelas no sistema. Esse componente facilita a exibição de dados de forma dinâmica e padronizada.
    
    Impacto: melhora a modularidade do código e padroniza a estrutura de tabelas no projeto.
    
    Arquivos criados:
    
    app/View/Components/TableBrowser.php
    
    resources/views/components/table-browser.blade.php

---
+++ resources/views/components/table-browser.blade.php
@@ -0,0 +1,23 @@
+        <!-- Navegação entre tabelas -->
+        <div class="inline-flex items-center border border-gray-400 rounded-lg overflow-hidden shadow-md bg-white mt-4">
+            <!-- Botão Anterior -->
+            <button class="px-4 py-2 border-r border-gray-400 text-black flex items-center transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-200 active:scale-95 hover:text-yellow-500 hover:font-bold">
+                <i class="fas fa-chevron-left mr-2"></i> Anterior
+            </button>
+
+            <!-- Páginas -->
+            <button class="px-4 py-2 border-r border-gray-400 text-black transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-200 active:scale-95 hover:text-yellow-500 hover:font-bold">
+                1
+            </button>
+            <button class="px-4 py-2 border-r border-gray-400 text-black transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-200 active:scale-95 hover:text-yellow-500 hover:font-bold">
+                2
+            </button>
+            <button class="px-4 py-2 border-r border-gray-400 text-black transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-200 active:scale-95 hover:text-yellow-500 hover:font-bold">
+                3
+            </button>
+
+            <!-- Botão Próximo -->
+            <button class="px-4 py-2 text-black flex items-center transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-200 active:scale-95 hover:text-yellow-500 hover:font-bold">
+                Próximo <i class="fas fa-chevron-right ml-2"></i>
+            </button>
+        </div>
